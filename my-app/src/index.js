c429cd99f8d7ab17f6a12b336e35ffa45ffa525d
avery.Chae <chaehj91@gmail.com>
2023-01-18 18:19:25 +0900

    20230118

---
+++ my-app/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import JsxUse from './chapter3/3.4/JsxUse';
+import Library from './chapter3/Library';
+import Clock from './chapter4/Clock';
+import PropUse from './chapter5/5.3/PropUse';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
+
+
+// 3장 예제
+// root.render(
+//   // <JsxUse/>
+//   <Library />
+// )
+
+
+// 4장 예제
+// 1초마다 Clock 컴포넌트를 root div에 렌더링하는 코드
+// setInterval(() => {
+//   root.render(
+//     <Clock />
+// );
+// }, 1000);
+
+
+// 5장 예제
+root.render(
+  <PropUse />
+);
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
